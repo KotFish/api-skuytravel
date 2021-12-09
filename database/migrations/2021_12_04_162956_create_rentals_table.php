8475e22dcb615c88103a81478a64f7ffe5321d58
GFigo <190710060@students.uajy.ac.id>
2021-12-10 00:27:24 +0700

    uwow

--- database/migrations/2021_12_04_162956_create_rentals_table.php
+++ database/migrations/2021_12_04_162956_create_rentals_table.php
@@ -19,7 +19,7 @@ class CreateRentalsTable extends Migration
             $table->string('nama_kendaraan');
             $table->string('jenis_kendaraan');
             $table->string('biaya_penyewaan');
-            $table->boolean('status');
+            $table->integer('status');
             $table->timestamps();
         });
     }
